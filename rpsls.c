34a7b8b45d02158397fb6a40794de3f9600ff544
SonicDirewolf <33938563+SonicDirewolf@users.noreply.github.com>
2018-10-01 22:33:47 +0530

    Update User Interface
    Choice Option added for user to input

--- rpsls.c
+++ rpsls.c
@@ -123,7 +123,9 @@ int rpsls(const char *player1, const char *player2){
 
 //int main (void)
 //{
-//    printf ("%d\n", rpsls("rock","paper"));
+//   printf("Enter Choice in numbers (1-4) :");
+
+//     printf ("%d\n", rpsls("rock","paper"));
 //    printf ("%d\n", rpsls("rock","rock"));
 //    printf ("%d\n", rpsls("paper","rock"));
 //    printf ("%d\n", rpsls("lizard",(char*)0));
